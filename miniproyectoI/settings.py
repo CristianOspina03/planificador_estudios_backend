c38047f255f611ff1b9068d43862fdf22a48ecef
juzer1711 <juzer1711@gmail.com>
2026-02-27 14:09:12 -0500

    Docs preparando el deploy para render

--- miniproyectoI/settings.py
+++ miniproyectoI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z(r8hg6(g4@cgdl8ztc4z=(#w_ti+270f48*&iku%3^v^8yp4m'
+#SECRET_KEY = 'django-insecure-z(r8hg6(g4@cgdl8ztc4z=(#w_ti+270f48*&iku%3^v^8yp4m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
