a769ee57f90f2b7ae9f7454fb5cb43da112e571e
juzer1711 <juzer1711@gmail.com>
2026-02-27 13:31:47 -0500

    fix formateo backend

--- actividades/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Actividad
-
-class ActividadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Actividad
-        fields = '__all__'
