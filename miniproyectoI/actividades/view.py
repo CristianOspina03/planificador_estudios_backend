0a5b90df9491073b7bd46f2393bf20f9b7222968
juzer1711 <juzer1711@gmail.com>
2026-03-03 20:59:56 -0500

    fix url

--- miniproyectoI/actividades/view.py
+++
@@ -1,9 +0,0 @@
-# views.py
-
-from rest_framework import viewsets
-from .models import Actividad
-from .serializers import ActividadSerializer
-
-class ActividadViewSet(viewsets.ModelViewSet):
-    queryset = Actividad.objects.all()
-    serializer_class = ActividadSerializer
