a93ef1995242fb1220e53e136dbbf4c26c0ee3f5
Simon <simonsevelascos@gmail.com>
2026-04-15 11:14:09 -0500

    Fix manual: agregando CORS y requirements

--- miniproyectoI/settings.py
+++ miniproyectoI/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -155,3 +155,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
